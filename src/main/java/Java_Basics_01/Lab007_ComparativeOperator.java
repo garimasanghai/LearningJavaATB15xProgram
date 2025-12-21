70192df9deec4e71392b5c771723bc98c4f704a4
garima <garima.sanghai@gmail.com>
2025-12-21 18:27:48 +0530

    Updated with recent changes

---
+++ src/main/java/Java_Basics_01/Lab007_ComparativeOperator.java
@@ -0,0 +1,31 @@
+package Java_Basics_01;
+
+public class Lab007_ComparativeOperator {
+    public static void main(String[] args) {
+        int a = 80;
+        int b= 70;
+        int c =60;
+
+        System.out.println("a > b = " + (a>b));
+        System.out.println("b > c = " + (b>c));
+        System.out.println("a > c = " + (a>c));
+        System.out.println("b > a = " + (b>a));
+        System.out.println("c > b = " + (c>b));
+        System.out.println("c > a = " + (c>a));
+
+
+        System.out.println("*****************");
+
+        boolean d = true;
+        boolean e = false;
+        System.out.println("d && e = " + (d && e));
+        System.out.println("e && e = " + (e && e));
+        System.out.println("d && d = " + (d && d));
+        System.out.println("d || e = " + (d || e));
+        System.out.println("d || d = " + (d || d));
+        System.out.println("e || d = " + (e || d));
+        System.out.println("!e : " + (!e));
+        System.out.println("!d : " + (!d));
+
+    }
+}
