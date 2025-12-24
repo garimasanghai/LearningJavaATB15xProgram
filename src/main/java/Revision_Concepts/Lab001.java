e3d966ad4939739018ad3169d3af1ab9af659c4b
garima <garima.sanghai@gmail.com>
2025-12-24 22:01:28 +0530

    Updated with recent changes

---
+++ src/main/java/Revision_Concepts/Lab001.java
@@ -0,0 +1,7 @@
+package Revision_Concepts;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Shout");
+    }
+}
---
+++ src/main/java/Revision_Concepts/Lab003.java
@@ -0,0 +1,14 @@
+package Revision_Concepts;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int num = 27;
+
+        if (num % 2 ==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
