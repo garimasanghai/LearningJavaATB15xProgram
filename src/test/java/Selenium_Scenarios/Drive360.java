858f85bfd2ed561e10ac49cbb725e7fcbda631dc
garima <garima.sanghai@gmail.com>
2026-03-01 18:28:35 +0530

    Updated with recent changes

---
+++ src/test/java/Selenium_Scenarios/Drive360.java
@@ -0,0 +1,32 @@
+package Selenium_Scenarios;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Drive360 {
+    @Test
+    public void test_drive() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.idrive360.com/enterprise/login");
+        driver.findElement(By.id("username")).sendKeys("augtest_040823@idrive.com");
+        driver.findElement(By.id("password")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[contains(text(), 'Sign in')]")).click();
+        Thread.sleep(20000);
+
+        String txt = driver.findElement(By.xpath("//div[@id = 'expiredmsg']//h5")).getText();
+        Assert.assertEquals(txt, "Your free trial has expired!");
+        System.out.println("Error msg successfully displayed");
+
+        driver.quit();
+
+    }
+}
