d76827e2415439578a30ce819fea32ccead9acd7
garima <garima.sanghai@gmail.com>
2026-03-02 16:16:39 +0530

    Updated with recent changes

---
+++ src/test/java/Selenium_Scenarios/DemoQaWebTable.java
@@ -0,0 +1,71 @@
+package Selenium_Scenarios;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+//add 5 emp. Delete the first emp whose dept is legal
+
+public class DemoQaWebTable {
+    @Test
+    public void test1() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/webtables");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        try {
+            Thread.sleep(2000);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        driver.findElement(By.id("addNewRecordButton")).click();
+        try {
+            Thread.sleep(2000);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        WebElement firstName1 = driver.findElement(By.id("firstName"));
+        firstName1.sendKeys("Tory");
+        WebElement lastName1 = driver.findElement(By.id("lastName"));
+        lastName1.sendKeys("Burch");
+        WebElement email1 = driver.findElement(By.id("userEmail"));
+        email1.sendKeys("tory@example.com");
+        WebElement age1 = driver.findElement(By.id("age"));
+        age1.sendKeys("35");
+        WebElement salary1 =driver.findElement(By.id("salary"));
+        salary1.sendKeys("80000");
+        WebElement dept1 = driver.findElement(By.id("department"));
+        dept1.sendKeys("Legal");
+
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("addNewRecordButton")).click();
+        WebElement firstName2 = driver.findElement(By.id("firstName"));
+        firstName2.sendKeys("Tory1");
+        WebElement lastName2 = driver.findElement(By.id("lastName"));
+        lastName2.sendKeys("Burch1");
+        WebElement email2 = driver.findElement(By.id("userEmail"));
+        email2.sendKeys("tory1@example.com");
+        WebElement age2 = driver.findElement(By.id("age"));
+        age2.sendKeys("45");
+        WebElement salary2 =driver.findElement(By.id("salary"));
+        salary2.sendKeys("87000");
+        WebElement dept2 = driver.findElement(By.id("department"));
+        dept2.sendKeys("Finance");
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(2000);
+
+        driver.quit();
+
+
+    }
+}
---
+++ src/test/java/Selenium_Scenarios/WebTableDemoQA.java
@@ -0,0 +1,84 @@
+package Selenium_Scenarios;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class WebTableDemoQA {
+    WebDriver driver;
+
+    // Reusable method
+    public void addUser(String firstName, String lastName, String email, String age,
+                        String salary, String department) {
+
+        driver.findElement(By.id("firstName")).sendKeys(firstName);
+        driver.findElement(By.id("lastName")).sendKeys(lastName);
+        driver.findElement(By.id("userEmail")).sendKeys(email);
+        driver.findElement(By.id("age")).sendKeys(age);
+        driver.findElement(By.id("salary")).sendKeys(salary);
+        driver.findElement(By.id("department")).sendKeys(department);
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/webtables");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // -------- Employee 1 --------
+        driver.findElement(By.id("addNewRecordButton")).click();
+
+        addUser("Tory", "Burch","tory@example.com",
+                "35", "80000", "Legal");
+
+        Thread.sleep(2000);
+
+        // -------- Employee 2 --------
+        driver.findElement(By.id("addNewRecordButton")).click();
+
+        addUser("Tory1", "Burch1","tory1@example.com",
+                "45", "87000", "Finance");
+
+        Thread.sleep(2000);
+
+        // -------- Employee 3 --------
+        driver.findElement(By.id("addNewRecordButton")).click();
+
+        addUser("Tory2", "Burch2","tory2@example.com",
+                "29", "90000", "IT");
+
+        Thread.sleep(2000);
+
+        // -------- Employee 4 --------
+        driver.findElement(By.id("addNewRecordButton")).click();
+
+        addUser("Tory3", "Burch3","tory3@example.com",
+                "33", "69000", "Dev");
+
+        Thread.sleep(2000);
+
+        // -------- Employee 5 --------
+        driver.findElement(By.id("addNewRecordButton")).click();
+
+        addUser("Tory4", "Burch4","tory4@example.com",
+                "54", "77000", "Tester");
+
+        WebElement getEmpName = driver.findElement(By.xpath("//td[contains(text(), 'Legal')]/parent::tr/td[1]"));
+        System.out.println(getEmpName.getText());
+
+        WebElement delRecord = driver.findElement(By.id("delete-record-3"));
+        delRecord.click();
+
+        Thread.sleep(4000);
+        System.out.println("Test Exe");
+
+        driver.quit();
+    }
+}
