3f2d6b54bc118ca3516afcadc3061c1c39d9d4f9
garima <garima.sanghai@gmail.com>
2026-03-02 08:22:14 +0530

    Updated with recent changes

---
+++ src/test/java/Selenium_Scenarios/OrangeCRM_Demo01.java
@@ -0,0 +1,43 @@
+package Selenium_Scenarios;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OrangeCRM_Demo01 {
+    @Test
+    public void test_OHRM_login() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://awesomeqa.com/hr/web/index.php/auth/login");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement input_username = driver.findElement(By.xpath("//input[@name='username']"));
+        WebElement input_password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        WebElement button = driver.findElement(By.xpath("//button"));
+//        WebElement button = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+
+        input_username.sendKeys("admin");
+        input_password.sendKeys("Hacker@4321");
+        button.click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement h6 = driver.findElement(By.xpath("//span[@class=\"oxd-topbar-header-breadcrumb\"]/h6"));
+
+        Assert.assertEquals(h6.getText(), "PIM");
+
+
+        driver.quit();
+    }
+}
