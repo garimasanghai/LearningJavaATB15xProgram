421ad6c5b7ac58886e6fb97c302fd5de7545f8fe
garima <garima.sanghai@gmail.com>
2026-03-05 08:13:01 +0530

    Updated with recent changes

---
+++ src/test/java/Selenium_Explicit_Wait/WaitHelper.java
@@ -0,0 +1,28 @@
+package Selenium_Explicit_Wait;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.image.ByteLookupTable;
+import java.time.Duration;
+
+public class WaitHelper {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public WaitHelper(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public WebElement waitForElementToBeVisible(By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitForElementToBeClicked(By locator){
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+}
