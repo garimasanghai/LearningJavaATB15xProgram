d314ea5106102cdc12ad2fe20dd564c20ecc4858
garima <garima.sanghai@gmail.com>
2026-03-06 08:33:26 +0530

    Updated with recent changes

---
+++ src/test/java/Handling_Svg/SvgMap.java
@@ -0,0 +1,39 @@
+package Handling_Svg;
+
+import Base_Class.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SvgMap extends WaitHelpers {
+    @Test
+    public void verifyStatesInSVG(){
+        ChromeDriver driver = new ChromeDriver();
+        String URL = "https://www.amcharts.com/svg-maps/?map=india";
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+        WaitHelpers waitHelpers = new WaitHelpers();
+        waitHelpers.waitJVM(3000);
+
+        List<WebElement> states = driver.findElements(By.xpath("//*[name()='svg']/*[name()='g'][7]/*[name()='g']/*[name()='g']/*[name()='path']"));
+
+        for (WebElement state : states){
+
+            System.out.println(state.getDomAttribute("aria-label"));
+            // state.getAttribute(); Deprecated
+            if(state.getDomAttribute("aria-label").contains("Tripura")){
+                state.click();
+            }
+
+
+        }
+
+
+
+    }
+
+}
---
+++ src/test/java/Selenium_Explicit_Wait/MakeMyTrip.java
@@ -0,0 +1,24 @@
+package Selenium_Explicit_Wait;
+
+import Base_Class.CommonToAll;
+import Base_Class.WaitHelpers;
+import io.qameta.allure.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MakeMyTrip extends CommonToAll {
+    @Description("Verify  makemytrip opens with modal")
+    @Test
+    public void testCloseModal() {
+        WebDriver driver = new ChromeDriver();
+        openBrowser(driver,"https://www.makemytrip.com/");
+        waitForVisibility(driver,3,"//span[@data-cy='closeModal']");
+        clickElementFound("//span[@data-cy='closeModal']");
+
+        // static -
+        WaitHelpers.waitJVM(10000);
+        closeBrowser(driver);
+    }
+
+}
